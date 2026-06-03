f47f68e2a4237f9e92eee1678a2e6c24aa70d87d
jeevithraangel <jeevithraangel@gmail.com>
2026-06-03 14:54:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import AboutUs from "./components/AboutUs";
 import ProductList from "./components/ProductList";
 import CartItem from "./components/CartItem";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(false);
+
+  const handleGetStarted = () => {
+    setShowProducts(true);
+  };
+
   return (
     <BrowserRouter>
-      <div className="landing">
-
+      <div className="landing-page">
         <h1>Welcome to Paradise Nursery</h1>
 
         <Link to="/plants">
-          <button>Get Started</button>
+          <button onClick={handleGetStarted}>
+            Get Started
+          </button>
         </Link>
-
       </div>
 
       <Routes>
         <Route path="/" element={<AboutUs />} />
-        <Route path="/plants" element={<ProductList />} />
+        <Route
+          path="/plants"
+          element={showProducts ? <ProductList /> : <AboutUs />}
+        />
         <Route path="/cart" element={<CartItem />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
